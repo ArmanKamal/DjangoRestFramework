c6b97b1e5ad22f46e778f615bca3418813cc89e4
Arman Kamal <armankamal2421@gmail.com>
2020-09-21 00:14:43 -0700

    Added ClassBased Views For API

--- basic_api/urls.py
+++ basic_api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import article_list,article_detail
+from .views import article_list,article_detail,ArticleListView,ArticleDetailView
 
 urlpatterns = [
     path('', article_list),
-    path('<int:pk>', article_detail)
+    path('<int:pk>/', article_detail),
+    path('cbv/', ArticleListView.as_view()),
+    path('cbv/<int:id>', ArticleDetailView.as_view()),
+
 ]
