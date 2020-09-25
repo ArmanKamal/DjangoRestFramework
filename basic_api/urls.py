93f1881f34ca5d279ca44e0c980e84f34bd26817
Arman Kamal <armankamal2421@gmail.com>
2020-09-24 22:14:54 -0700

    Added ViewSet View

--- basic_api/urls.py
+++ basic_api/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
-from .views import article_list,article_detail,ArticleListView,ArticleDetailView,GenericeApiListView,GenericAPIDetailView
+from django.urls import path,include
+from .views import article_list,article_detail,ArticleListView,ArticleDetailView,GenericeApiListView,GenericAPIDetailView,ArticleViewSet    
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('',ArticleViewSet,basename='article')
 
 urlpatterns = [
     path('', article_list),
@@ -8,7 +12,9 @@ urlpatterns = [
     path('cbv/<int:id>', ArticleDetailView.as_view()),
     path('gv/',GenericeApiListView.as_view()),
     path('gv/<int:id>',GenericAPIDetailView.as_view()),
+    path('vs/',include(router.urls))
 
 
 
-]
+
+] 
--- basic_api/views.py
+++ basic_api/views.py
@@ -12,7 +12,36 @@ from rest_framework import generics
 from rest_framework import mixins
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication,TokenAuthentication
 from rest_framework.permissions import IsAuthenticated 
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+
+class ArticleViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles,many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer =  ArticleSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+
+    def retrieve(self, request, pk=None):
+       article = get_object_or_404(Article, pk=pk) 
+       serializer = ArticleSerializer(article)
+       return Response(serializer.data)
+
+
+
+
+
 
 
 ## GENERIC VIEW ##
